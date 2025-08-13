e74dd4690bda705a7501d5f15d92a36c89ee6d78
Nix <85513545+Niximkk@users.noreply.github.com>
2025-08-13 00:10:38 -0300

    [🌜] First commit!!

---
+++ NABS.user.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         Nix's Anti Blindness Shield
+// @namespace    https://github.com/Niximkk
+// @version      1.0.0
+// @description  Replaces the Basemap of the Wplace map
+// @author       Nix
+// @match        https://wplace.live/*
+// @grant        none
+// @icon         https://em-content.zobj.net/source/google/428/last-quarter-moon-face_1f31c.png
+// @license      GPL-3.0
+// @updateURL    https://raw.githubusercontent.com/Niximkk/Wplace-Anti-Blindness-Shield/main/NABS.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    /* Hi, if you want to modify the basemap to a custom one, change this Variable */
+      /* Oie, se quiser trocar o basemap pra um customizado, troca essa Variavel */
+             /* Below are some examples - Abaixo deixarei alguns exemplos */
+
+    // Default - https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json
+    // Void friendly (grey) - https://geoserveis.icgc.cat/contextmaps/icgc_mapa_base_fosc.json
+
+    const basemapJson = "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json";
+
+
+    /* ---  Dont mess with this part tho  --- */
+
+    const originalFetch = window.fetch;
+
+    window.fetch = async function(input, init) {
+        const url = input instanceof Request ? input.url : input;
+
+        if (url === "https://maps.wplace.live/styles/liberty") {
+            try {
+                const darkStyleResponse = await originalFetch(basemapJson);
+                const darkStyleData = await darkStyleResponse.json();
+
+                console.log(`🌙 Changed the basemap to ${basemapJson}.`);
+
+                return new Response(JSON.stringify(darkStyleData), {
+                    status: 200,
+                    statusText: "OK",
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                });
+            } catch (error) {
+                console.error("❌ Something went wrong with the basemap:", error);
+                return originalFetch.apply(this, arguments);
+            }
+        }
+
+        return originalFetch.apply(this, arguments);
+    };
+
+    console.log("🔧 Userscript loaded - Dark Mode Map active");
+})();
---
+++ README.md
@@ -0,0 +1,30 @@
+# Nix's Anti Blindness Shield
+### Now you can play [WPlace](https://wplace.live/) without loosing eye sight!
+
+<img src="https://em-content.zobj.net/source/google/428/last-quarter-moon-face_1f31c.png" width="128px" height="128px" alt="CutieMoon">
+
+## 🧪 Installation
+
+1. Download [Tampermonkey](https://www.tampermonkey.net/). 
+2. [Click me!](https://raw.githubusercontent.com/Niximkk/Wplace-Anti-Blindness-Shield/main/NABS.user.js).
+3. Hit install and open [WPlace](https://wplace.live/).
+
+## ❗ Notes
+
+- You can change the **JSON URL** to use a different map style.
+- This script SHOULD auto-update.
+
+---
+
+Copyleft (C) 2025 Nix
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+<p align="center">
+  <a href="https://emoji.gg/emoji/5349-hellokittybyebye">
+    <img src="https://cdn3.emoji.gg/emojis/5349-hellokittybyebye.png" width="128px" height="128px" alt="HelloKittyByeBye">
+  </a>
+</p>
